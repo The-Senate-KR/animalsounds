08e1fbaa380311e1cbb57486d59f1ee4b3d40bae
The-Senate-KR <thesenate639@gmail.com>
2025-06-23 14:09:42 +0100

    Added some more clarification to the error message in animal_sounds()

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -13,13 +13,15 @@ animal_sounds <- function(animal, sound){
   if(!rlang::is_character(animal, n = 1)){
     cli::cli_abort(
       c("{.var animal} must be a single string!",
-        "i" = "It was {.type {animal}} of length {length(animal)} instead.")
+        "i" = "It was {.type {animal}} of length {length(animal)} instead.",
+        "i" = "You typed {animal}")
     )
   }
   if(!rlang::is_character(sound, n = 1)){
     cli::cli_abort(
       c("{.var sound} must be a single string!",
-        "i" = "It was {.type {sound}} of length {length(sound)} instead.")
+        "i" = "It was {.type {sound}} of length {length(sound)} instead.",
+        "i" = "You typed {sound}")
     )
   }
   paste0("The ", animal, " says ", sound, "!")
