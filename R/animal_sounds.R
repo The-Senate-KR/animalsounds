d7dbd5deb4a9f7196f4b7a824cee83912038467c
The-Senate-KR <thesenate639@gmail.com>
2025-06-23 13:36:42 +0100

    Improved documentation of animal_sounds()

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,6 +1,6 @@
 #' Check Animal Sounds
 #'
-#' A function which allows you to vibe out whether various animals make a sound of your choice. This relies upon you having a vague awareness of what each animal sounds like.
+#' A function which allows you to vibe out whether various animals make a sound of your choice. This relies upon you having a vague awareness of what each animal sounds like. Makes use of the [base::paste0()] function to put _everything_ together (it's **basically** a wrapper).
 #'
 #' @param animal The animal that you're trying to work the sound out for.
 #' @param sound The sound you want to check against the animal.
